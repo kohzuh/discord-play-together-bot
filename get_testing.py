d694856a65c645567b1d9edf0ff85085c6843997
unknown <evankoza77@gmail.com>
2026-03-09 19:01:08 -0400

    formatting new methods seperately

--- get_testing.py
+++ get_testing.py
@@ -1,7 +1,17 @@
+import os
 import requests
 import json
 
-STEAM_API_KEY="50A0C901C8BE9578492C42A42785BC64"
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+uIDs=(76561198148289427, 76561198262549310, 76561198834362035, 76561199077420855)
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+STEAM_API_KEY = os.getenv("STEAM_API_KEY")
+
 MULTIPLAYER_CATEGORY_NAMES = (
     "Multi-player",
     "Multiplayer",
@@ -12,6 +22,10 @@ MULTIPLAYER_CATEGORY_NAMES = (
     "mmo"
 )
 
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
 def get_app_categories(appid: str):
     url = "https://store.steampowered.com/api/appdetails"
     params = {
@@ -19,12 +33,13 @@ def get_app_categories(appid: str):
         "cc": "ca",
         "l": "en",
     }
+    try:
+        response = requests.get(url, params=params, timeout=10).json()
+        categories = set(item["description"] for item in response[str(appid)]["data"]["categories"])
 
-    response = requests.get(url,params).json()
-
-    categories = set(item["description"] for item in response[str(appid)]["data"]["categories"])
-
-    return categories
+        return categories
+    except:
+        return set()
 
 def is_multiplayer_game(appid: str):
     categories = get_app_categories(appid)
@@ -38,11 +53,13 @@ def get_friend_list(steamid: str):
         "steamid": steamid,
         "relationship": "friend",
     }
-    response = requests.get(url, params).json()
+    try:
+        response = requests.get(url, params=params, timeout=10).json()
 
-    friends_list = [item["steamid"] for item in response["friendslist"]["friends"]]
-
-    return friends_list
+        friends_list = [item["steamid"] for item in response["friendslist"]["friends"]]
+        return friends_list
+    except:
+        return []
 
 def get_library(steamid: str):
     url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/"
@@ -52,16 +69,22 @@ def get_library(steamid: str):
         "include_appinfo": "true",
         "include_played_free_games": "true",
     }
-    response = requests.get(url,params).json()
-
-    game_list = set((item["appid"], item["name"]) for item in response["response"]["games"])
-
-    return game_list
+    try:
+        response = requests.get(url, params=params, timeout=10).json()
+        game_list = set((item["appid"], item["name"]) for item in response["response"]["games"])
+    
+        return game_list
+    except:
+        return set()
 
 def get_shared_library(users):
     libraries=[]
+
     for user in users:
-        libraries.append(get_library(user))
+        library = get_library(user)
+        if library:
+            libraries.append(get_library(library))
+
     shared_library = set.intersection(*libraries) if libraries else set()
 
     multi_library=set()
@@ -69,4 +92,4 @@ def get_shared_library(users):
         if is_multiplayer_game(game[0]):
             multi_library.add(game)
 
-    return shared_library
+    return multi_library
